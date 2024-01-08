0058a863d9d5a89edc114c8945c0da9fc935cbdf
solverharsh <harshdeepk30@gmail.com>
2024-01-08 13:46:39 +0530

    changed snippet

--- pythonCasting.py
+++ pythonCasting.py
@@ -2,7 +2,7 @@
 
 # int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
 # float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
-# str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
+# str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals.
 
 x = int(1)
 y= int(3.9)
