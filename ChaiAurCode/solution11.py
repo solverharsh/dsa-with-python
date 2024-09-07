d66e0c700d854dea12508c2574497ce2310fc81a
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2024-09-07 12:58:03 +0000

    added while logic in solution 11

--- ChaiAurCode/solution11.py
+++ ChaiAurCode/solution11.py
@@ -1,9 +1,15 @@
-# Compute the factorial of number using whille loop ;
+# Compute the factorial of number using for loop && whille loop ;
 
 # 5! = 5*4*3*2*1
 
 num = int(input("Enter the number : "))
 fact = 1
-for i in range(2 , num+1):
-    fact*=i
+
+# for i in range(2 , num+1):
+#     fact*=i
+# print(f"The factorial of given number is {fact}")
+
+while num>0:
+    fact*=num
+    num-=1
 print(f"The factorial of given number is {fact}")
