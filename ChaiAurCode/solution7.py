7f4643fd1fee39104096d4e28f159a4af3d5d315
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2024-09-07 11:49:47 +0000

    2nd for loop solution added

---
+++ ChaiAurCode/solution7.py
@@ -0,0 +1,10 @@
+# Calculate the sum of even numbers upto a given number n ;
+n =int(input("Enter the value of n : "))
+sum_even = 0
+for num in range(1 , n+1):
+    if num%2 == 0:
+        sum_even+=num
+print(f"Sum of even numbers till n is : {sum_even}")
+
+# n =11
+# 2+4+6+8+10
