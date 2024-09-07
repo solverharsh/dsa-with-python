5f1a51021c36b23fa8bdaeafb4a097e666a5c9ca
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2024-09-07 17:07:20 +0000

    basic function syntax added

---
+++ ChaiAurCode/Functions/function.py
@@ -0,0 +1,13 @@
+#Basic function Syntax ;
+
+def sum(a ,b):
+    return a + b
+
+val = sum(4,5)  # function call ;
+print(val)
+
+def square(num):
+    return num**2
+
+result = square(6)   # function call ;
+print(result) 
